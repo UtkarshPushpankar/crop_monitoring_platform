cf401e92a0d306f9c99e2415d305c2ed47efc13b
Utkarsh Pushpankar <upushpankar@gmail.com>
2025-09-19 00:52:12 +0530

    Added routing using react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import Upload from './uploadImage/uplaod'
 import Chatbot from './Chatbot/Chatbutton'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-red-400'>
-        Hello, Crop Monitoring Platform is live.
-        <Upload/>
-        <Chatbot/>
+      <div>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
       </div>
     </>
   )
